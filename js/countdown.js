d9ade5c5c96a76381ffe83582a0085ee85947fa2
Lianynka <yakovchuk00y@gmail.com>
2022-03-25 20:20:42 +0000

    added countdown

--- js/countdown.js
+++ js/countdown.js
@@ -1,27 +1,33 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
 
-    let countdown; 
-    const timerDislaySeconds = document.querySelector('.countdown-num-sec');
-    const timerDislayMinutes = document.querySelector('.countdown-num-min');
+const currentYear = new Date().getFullYear();
+const targetTime = new Date(`May 31 ${
+    currentYear
+} 00:00:00`);
 
-    
-    function timer(seconds){
-        const now = Date.now();
-        const then = now + seconds * 1000; 
-        displayTimeLeft(seconds)
-        countdown = setInterval (() => {
-            const secondsLeft = Math.round((then - Date.now()) / 1000);
-            if(secondsLeft < 0 ){
-                clearInterval(countdown)
-                return 
-            }
-            displayTimeLeft(secondsLeft)
-        }, 1000) 
-    }
-    function displayTimeLeft (seconds){
-       
-        const restminutes = Math.floor(seconds / 60); 
-        const remainderSeconds = seconds % 60;
-        const displaySeconds = `${remainderSeconds}`;
-        timerDislaySeconds.textContent = displaySeconds; 
-        
-    }
+function updateCountdown() {
+    const currentTime = new Date();
+    const diff = targetTime - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = 'flex';
+}, 1000);
+
+setInterval(updateCountdown, 1000);
--- js/try.js
+++ js/try.js
@@ -3,16 +3,47 @@ var countDownDate = new Date("Jul 25, 2022 16:37:52").getTime();
 var myfunc = setInterval(function timer() {
     var now = new Date().getTime();
     var timeleft = countDownDate - now;
-        
+
     var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
     var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+}, 1000)
+
+const days = Math.floor(seconds / 86400)
+const hours = Math.floor(seconds / 3600)
+
+const timerDislayHours = document.querySelector('.countdown-num-hour');
+const timerDislayDays = document.querySelector('.countdown-num-day');
+
+const loading = document.getElementById('loading');
+
+let countdown; 
+const timerDislaySeconds = document.querySelector('.countdown-num-sec');
+const timerDislayMinutes = document.querySelector('.countdown-num-min');
+
+
+function timer(seconds){
+    const now = Date.now();
+    const then = now + seconds * 1000; 
+    displayTimeLeft(seconds)
+    countdown = setInterval (() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        if(secondsLeft < 0 ){
+            clearInterval(countdown)
+            return 
+        }
+        displayTimeLeft(secondsLeft)
+    }, 1000) 
+}
+function displayTimeLeft (seconds){
+   
+    const restminutes = Math.floor(seconds / 60); 
+    const remainderSeconds = seconds % 60;
+    const displaySeconds = `${remainderSeconds}`;
+    timerDislaySeconds.textContent = displaySeconds; 
     
-    }, 1000)
+}
 
-    const days = Math.floor( seconds/ 86400)
-    const hours = Math.floor(seconds / 3600)
 
-    const timerDislayHours = document.querySelector('.countdown-num-hour');
-    const timerDislayDays = document.querySelector('.countdown-num-day');
