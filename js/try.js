296d67a6e70e6c2204be3906677ddf8831698b0e
Lianynka <yakovchuk00y@gmail.com>
2022-03-25 14:10:26 +0000

    unsuccessful js file

---
+++ js/try.js
@@ -0,0 +1,18 @@
+var countDownDate = new Date("Jul 25, 2022 16:37:52").getTime();
+
+var myfunc = setInterval(function timer() {
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+        
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+    }, 1000)
+
+    const days = Math.floor( seconds/ 86400)
+    const hours = Math.floor(seconds / 3600)
+
+    const timerDislayHours = document.querySelector('.countdown-num-hour');
+    const timerDislayDays = document.querySelector('.countdown-num-day');
